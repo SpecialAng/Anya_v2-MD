1e55b1e057462b1515967de22d27a2a324ae1439
SpecialAng <suicidezoey009@gmail.com>
2025-01-09 22:06:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://zoey12334:<A2K_82VJKcjZDdU>@cluster0.sfldr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    sessionId: process.env.SESSION_ID || "X4FAh70mj_A_N_Y_A_V_2_sF6lRDhSIO0",
 
     
     
